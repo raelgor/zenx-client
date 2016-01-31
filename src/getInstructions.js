21f5c98abbd0cdf8f33aaa2416e4f58ef05915af
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2016-01-31 18:49:20 +0200

    Added some logging

--- src/getInstructions.js
+++ src/getInstructions.js
@@ -5,15 +5,26 @@ const makeMongoUrl = require('./makeMongoUrl');
 const mongodb = require('mongodb');
 const fs = require('fs');
 
+const log = (...args) => console.log('[' + new Date() + '] ', ...args);
+
 function getInstructions(config){
+    
+    log('getting instructions...');
    
     const mongoUrl = makeMongoUrl(config.mongodb);
     
     mongodb.connect(mongoUrl, (error, db) => {
         
+        if(error)
+            return log('error getting instructions.', error);
+            
+        log('connected to mongodb. loading instructions...')
+        
         db.collection('clients').find({ id: config.id }).toArray((err, data) => {
             
             let newConfig = data[0];
+            
+            log('loaded instructions for ' + newConfig.services.lengt + ' services. starting...');
             
             newConfig.initialized = true;
             
@@ -21,6 +32,8 @@ function getInstructions(config){
             
             for(let service in newConfig.services)
                 handlers[service.type](service);
+                
+            log('client operational.');
             
         });
         
