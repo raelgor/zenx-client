39cb0053a85045d655da47964fcf15399872a7e1
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2016-01-31 18:23:59 +0200

    Added getInstructions

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
+/* global handlers */
 'use strict';
 
 const config = require('./config');
 const mongodb = require('mongodb');
-const makeMongoUrl = require('./src/makeMongoUrl');
 const getInstructions = require('./src/getInstructions');
 
-const handlers = {
+global.handlers = {
     "load-balancer": require('./src/lb'),
     "eval": require('./src/eval')
 }
 
 if(!config.initialized)
-    getInstructions();
+    getInstructions(config);
 else
     for(let service in config.services)
         handlers[service.type](service);
---
+++ src/getInstructions.js
@@ -0,0 +1,32 @@
+/* global handlers */
+'use strict';
+
+const makeMongoUrl = require('./src/makeMongoUrl');
+const mongodb = require('mongodb');
+const fs = require('fs');
+
+function getInstructions(config){
+   
+    const mongoUrl = makeMongoUrl(config.mongodb);
+    
+    mongodb.connect(mongoUrl, (error, db) => {
+        
+        db.collection('clients').find({ id: config.id }).toArray((err, data) => {
+            
+            let newConfig = data[0];
+            
+            newConfig.initialized = true;
+            
+            fs.writeFileSync('./../config.js', JSON.stringify(newConfig));
+            
+            for(let service in newConfig.services)
+                handlers[service.type](service);
+            
+        });
+        
+    });
+    
+    
+}
+
+module.exports = getInstructions;
